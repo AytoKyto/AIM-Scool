1c9b5b8b4da2343f4472a3e538962d1807ce242d
Ayto <m.fleury942@gmail.com>
2021-09-11 17:49:35 +0200

    init

---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bigWrapper" v-if="cross">
+    <div class="overlay"></div>
+    <div class="wrapperModal">
+      <div class="modal">
+        <p><strong>Your parameters</strong></p>
+        <label>Circles size </label>
+        <input v-model="large" type="number" class="sizeInput" />
+        <br />
+        <br />
+        <label>Scores limit </label>
+        <input v-model="limitScore" type="number" class="sizeInput" />
+        <br />
+        <br />
+        <button @click="crossF" class="btn-modal">Send</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["cross", "crossF", "large", "limitScore"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapperModal {
+  position: fixed;
+  left: 25%;
+  width: 50vw;
+  padding: 2vw;
+  z-index: 999;
+  background-color: white;
+}
+.overlay {
+  background-color: rgba(0, 0, 0, 0.495);
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Shotvue.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="wrapper">
+    <Modal
+      :cross="cross"
+      :crossF="crossF"
+      :large="large"
+      :limitScore="limitScore"
+    />
+    <div class="shotWrapper">
+      <div :style="styleCirlce" @click="changeDirection" class="circle"></div>
+    </div>
+    <p class="score">Votre score et de {{ scoreVal }}</p>
+  </div>
+</template>
+
+<script>
+import Modal from "./Modal.vue";
+
+export default {
+  name: "Shotvue",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      scoreVal: 0,
+      limitScore: 20,
+      x: 0,
+      y: 0,
+      large: 20,
+      cross: true,
+    };
+  },
+  methods: {
+    changeDirection: function () {
+      this.scoreVal++;
+      if (this.scoreVal < this.limitScore) {
+        console.log(this.scoreVal);
+        this.x = Math.random() * 80;
+        this.y = Math.random() * 70;
+      } else {
+        alert("bravo");
+      }
+    },
+    crossF: function () {
+      this.cross = !this.cross;
+    },
+  },
+  computed: {
+    styleCirlce: function () {
+      return {
+        marginLeft: this.x + "vw",
+        marginTop: this.y + "vh",
+        width: this.large + "px",
+        height: this.large + "px",
+      };
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.shotWrapper {
+  border: 4px solid black;
+  width: 90vw;
+  height: 80vh;
+  margin: 0 auto;
+}
+.circle {
+  border: 1px solid black;
+  border-radius: 1000000px;
+  background-color: red;
+}
+</style>
